e3f9b911e7b3bc80d5e2f1d0b9ca61e7a47227aa
Daniel-Tenzler <reffrankdaniel@gmail.com>
2025-07-27 18:29:17 +0200

    remove deprecated code

--- index.js
+++
@@ -1,9 +0,0 @@
-import { app, PORT } from './src/config/app.js';
-import sensorRoutes from './src/routes/sensorRoutes.js';
-
-// Register routes
-app.use('/', sensorRoutes);
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
--- src/utils/hash.js
+++
@@ -1,5 +0,0 @@
-import crypto from 'crypto';
-
-export const sha256 = (message) => {
-    return crypto.createHash('sha256').update(message).digest('hex');
-};
