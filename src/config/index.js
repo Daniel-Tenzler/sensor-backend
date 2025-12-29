36b3679dfded4d49c2f3c5f5e2a32abf2d9a654a
Daniel-Tenzler <reffrankdaniel@gmail.com>
2025-12-29 09:04:37 +0100

    fix env issue

--- src/config/index.js
+++ src/config/index.js
@@ -3,8 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-	
-    PORT: process.env.PORT || 3000
+  PORT: process.env.PORT || 3000,
+  API_SECRET: process.env.API_SECRET || 'your_shared_secret_here',
+  DATABASE_URL: process.env.DATABASE_URL
 };
 
 export default config;
